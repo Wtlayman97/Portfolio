fff7a4b77702a72a931ca1b7610c99c81e6071e7
Wtlayman97 <108591815+Wtlayman97@users.noreply.github.com>
2024-07-03 16:53:47 -0400

    Dark Mode
    Added dark mode functionality and some minor css changes.
    Adjusted logo angle

--- PortfolioScript.js
+++ PortfolioScript.js
@@ -1,7 +1,30 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+const darkModeToggle = document.querySelector('.switch input');
+const body = document.body;
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('show');
     hamburger.classList.toggle('active');
 });
+
+darkModeToggle.addEventListener('change', ()=>{
+    if (darkModeToggle.checked){
+        body.classList.add('dark');
+        body.classList.remove('light');
+        logo.src = 'L97LogoFinal_dark.png';
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        body.classList.remove('dark');
+        body.classList.add('light');
+        logo.src = 'L97LogoFinal.png';
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+    body.classList.add('dark');
+    body.classList.remove('light')
+    darkModeToggle.checked = true;
+    logo.src = 'L97LogoFinal_dark.png';
+}
